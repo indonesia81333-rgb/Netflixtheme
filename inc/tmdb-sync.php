e5ee9ed203d72f0a15a160d500a59cd92b79e44a
indonesia81333-rgb <indonesia81333@gmail.com>
2026-06-02 16:21:03 +0530

    Update tmdb-sync.php

--- inc/tmdb-sync.php
+++ inc/tmdb-sync.php
@@ -154,4 +154,15 @@ class TMDB_Sync {
         
         require_once(ABSPATH . 'wp-admin/includes/media.php');
         require_once(ABSPATH . 'wp-admin/includes/file.php');
-        require
+        require_once(ABSPATH . 'wp-admin/includes/image.php');
+        
+        $attachment_id = media_sideload_image($image_url, $post_id, null, 'id');
+        
+        if(!is_wp_error($attachment_id)) {
+            set_post_thumbnail($post_id, $attachment_id);
+        }
+    }
+}
+
+new TMDB_Sync();
+?>
